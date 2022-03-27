b0f256c580c443bf105f9a6118d99f0f733c82d4
bhavesh <bhavesh.daswani@toptal.com>
2022-03-27 19:37:46 +0530

    Imported power service in Disk module

--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerService: PowerService) {}
+
+  getData() {
+    console.log(`To get data we need 5 watts of power`);
+    this.powerService.supplyPower(5);
+    return `Some Random Data`;
+  }
+}
