3b7f5cd65ba2187fa46e8fecc58103b98cdd9319
bhavesh <bhavesh.daswani@toptal.com>
2022-03-27 19:35:04 +0530

    Added compute method to Cpu Service

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -4,4 +4,10 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
   constructor(private powerService: PowerService) {}
+
+  compute(num1: number, num2: number) {
+    console.log(`For Compute we need 5 watts of power`);
+    this.powerService.supplyPower(10);
+    return num1 + num2;
+  }
 }
